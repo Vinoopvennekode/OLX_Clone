f40f4828b59e0b48d0d3783c728e93ea6bc4f2bb
vinoop vennekode <vinoopvennekode@gmail.com>
2023-01-18 14:56:08 +0530

    user sign up,sign in,create post ,post list

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import {FirebaseContext}from './store/context'
+import firebase from './firebase/config';
+import Context from './store/context'
+ReactDOM.render(
+<FirebaseContext.Provider value={{firebase}}>
+<Context>
+ <App />
+</Context>
+</FirebaseContext.Provider>
+, document.getElementById('root'));
